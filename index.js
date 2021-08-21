2765fba5f84299be82b83d5747170dc53b18a12b
sameena <sameenakhanum007@gmail.com>
2021-08-21 09:40:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+let parentEl = document.getElementById("parent")
+console.log(parentEl);
+
+let parentE2 = document.getElementById("parent")
+console.log(parentE2.firstChild);
+
+let parentE3 = document.getElementById("parent")
+console.log(parentE3.firstElementChild);
+
+let parentE4 = document.getElementById("parent")
+console.log(parentE4.lastChild);
+
+let parentE5 = document.getElementById("parent")
+console.log(parentE5.lastElementChild);
+
+let parentE6 = document.getElementById("parent")
+console.log(parentE6.childNodes);
+
+let parentE7 = document.getElementById("parent")
+console.log(parentE7.children[4]);
+
+let midEl = document.getElementById("midparagraph")
+console.log(midEl);
+
+let midE2 = document.getElementById("midparagraph")
+console.log(midE2.nextSibling);
+
+let midE3 = document.getElementById("midparagraph")
+console.log(midE3.nextElementSibling);
+
+let midE4 = document.getElementById("midparagraph")
+console.log(midE4.previousElementSibling);
